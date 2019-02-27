182020c127aca9bd0ce4ee7d303c50eac8eb9fa1
barbabjetolov <avskyrb@gmail.com>
2019-02-27 18:11:21 +0100

    trigonometrics in degrees module

---
+++ modules/trigonometrics_in_degrees.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def sin(angle_in_degrees):
+    angle_in_radians = np.radians(angle_in_degrees)
+    return np.sin(angle_in_radians)
+
+def cos(angle_in_degrees):
+    angle_in_radians = np.radians(angle_in_degrees)
+    return np.cos(angle_in_radians)
+
+def tan(angle_in_degrees):
+    angle_in_radians = np.radians(angle_in_degrees)
+    return np.tan(angle_in_radians)
